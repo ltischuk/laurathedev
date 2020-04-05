6339e5d168b70fe79c9d1cd671d8bbeec0870568
Laura Holbrook <laura.holbrook@accesso.com>
2020-04-05 17:18:07 -0400

    home page edits

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home/home.component';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatDividerModule } from '@angular/material/divider';
 
 @NgModule({
-  imports: [CommonModule, HomeRoutingModule, FlexLayoutModule, HomeRoutingModule],
+  imports: [CommonModule, HomeRoutingModule, FlexLayoutModule, HomeRoutingModule, MatDividerModule],
   declarations: [HomeComponent],
 })
 export class HomeModule {}
--- src/app/pages/home/home/home.component.ts
+++ src/app/pages/home/home/home.component.ts
@@ -1,15 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  animations: [trigger('flyIn', [state('in', style({ transform: 'translateX(0)' })), transition('void => *', [style({ transform: 'translateX(-100%)' }), animate('900ms ease-in')])])],
 })
 export class HomeComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
